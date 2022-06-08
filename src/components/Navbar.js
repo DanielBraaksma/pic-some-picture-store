16db925ddc4d009f1b74b335d40df6df66d86252
Daniel Braaksma <danebraaksma@hotmail.com>
2022-06-07 20:38:11 -0400

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+// import React from "react"
+// import Logo from "../images/logo.png"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <h1>Test</h1>
+        </nav>
+    )
+}
