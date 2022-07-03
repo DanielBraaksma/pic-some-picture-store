4e69e7a52a7d845991e93de36c8572bf1abbcb40
Daniel Braaksma <danebraaksma@hotmail.com>
2022-07-03 13:56:52 -0400

    fixed hovered display logic on image

--- src/components/Image.js
+++ src/components/Image.js
@@ -2,7 +2,10 @@ import React, {useState} from "react"
 
 function Image({className, img}) {
     const [hovered, setHovered] = useState(false)
-    console.log(hovered)
+    
+    const heartIcon = hovered && <i className="ri-heart-line favorite"></i>
+    const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>
+
     return (
         <div
             className={`${className} image-container`}
