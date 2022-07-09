2ac0702f0990baa8256d8a7d922f5a5e341185a5
Daniel Braaksma <danebraaksma@hotmail.com>
2022-07-09 09:32:52 -0400

    remove from cart feature added

--- src/Context.js
+++ src/Context.js
@@ -28,10 +28,12 @@ function ContextProvider({children}) {
         setCartItems(prevItems => [...prevItems, newItem])
     }
 
-    console.log(cartItems)
+    function removeFromCart(id){
+        setCartItems(prevItems => prevItems.filter(item=> item.id !== id))
+    }
 
     return (
-        <Context.Provider value={{allPhotos, toggleFavorite, addToCart}}>
+        <Context.Provider value={{allPhotos, toggleFavorite, removeFromCart, addToCart, cartItems}}>
             {children}
         </Context.Provider>
     )
